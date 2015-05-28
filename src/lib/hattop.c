4e81e2f2f4e67caaf7fc8222368404cd82ca4039
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2015-05-28 19:42:43 +0200

    Read full request

--- src/lib/hattop.c
+++ src/lib/hattop.c
@@ -4,6 +4,8 @@
 #include "hattop.h"
 #include "socket.h"
 
+#define REQUEST_BUF_SIZE 8192
+
 const char* hattop_asci_logo = "\
   ___   \n\
  |   |  \n\
@@ -34,14 +36,32 @@ int hattop_listen(hattop_t *state, short portno)
 {
     socket_t s;
     s = SOCKET_create(portno);
+    if(!SOCKET_is_valid(s)) {
+        return -1;
+    }
 
     while(1) {
         socket_t s_client = SOCKET_accept(s, 1000);
         if(SOCKET_is_valid(s_client)) {
             /* Dummy handler for testing */
             char head[] = "HTTP/1.1 200\r\n\r\n";
-            char recv_buf[8192];
-            SOCKET_recv(s_client, recv_buf, sizeof(recv_buf));
+            char recv_buf[REQUEST_BUF_SIZE];
+            int bytes_read = 0;
+
+            /* Read request */
+            fprintf(stderr, "\n");
+            do {
+                int ret = SOCKET_recv(s_client, &recv_buf[bytes_read], REQUEST_BUF_SIZE - bytes_read - 1);
+                if(ret > 0) {
+                    bytes_read += ret;
+                    recv_buf[bytes_read] = '\0';
+                    if(strstr(recv_buf, "\r\n\r\n")) {
+                        fprintf(stderr, "%s", recv_buf);
+                        break;
+                    }
+                }
+            } while(bytes_read < REQUEST_BUF_SIZE - 1);
+
             SOCKET_send(s_client, head, strlen(head));
             SOCKET_send(s_client, hattop_asci_logo, strlen(hattop_asci_logo));
             SOCKET_close(s_client);
