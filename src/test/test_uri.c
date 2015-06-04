3f48be8bc5a2fb0bfd81a002380388874b905386
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2015-06-04 15:41:35 +0200

    mocking up hattest - a minimalistic test lib

--- src/test/test_uri.c
+++ src/test/test_uri.c
@@ -1,8 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "uri.h"
 
+enum hattest_status {
+    HATTEST_PASS = 0,
+    HATTEST_FAIL = -1
+};
+
+static int test_id = 0;
+#define RUN_TEST(foo, ...) test_id++; printf( "%i: " #foo "("#__VA_ARGS__") ", test_id); if(foo(#__VA_ARGS__) != 0){printf("FAIL\n"); exit(test_id);}else{printf("PASS\n");}
+
+enum hattest_status bar(){
+    return HATTEST_PASS;
+}
+
 int main()
 {
+    RUN_TEST(bar);
     return 0;
 }
